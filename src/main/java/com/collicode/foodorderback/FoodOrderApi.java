b7e8c79c55a65e496e234cb3ed77a5582788b18a
c-kiplimo <kiplimocollins855@gmail.com>
2023-06-22 13:16:05 +0300

    chore:pom.xml refactor:rename class name

--- src/main/java/com/collicode/foodorderback/FoodOrderApi.java
+++ src/main/java/com/collicode/foodorderback/FoodOrderApi.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodOrderBackApplication {
+public class FoodOrderApi {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FoodOrderBackApplication.class, args);
+		SpringApplication.run(FoodOrderApi.class, args);
 	}
 
 }
