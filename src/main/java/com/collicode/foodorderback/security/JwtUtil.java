e2226e7465af4b9edc9d15ba9a9efe75c7675cc3
c-kiplimo <kiplimocollins855@gmail.com>
2023-06-22 21:31:18 +0300

    update secret key

--- src/main/java/com/collicode/foodorderback/security/JwtUtil.java
+++ src/main/java/com/collicode/foodorderback/security/JwtUtil.java
@@ -19,7 +19,7 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
 
-    private String secret = "javatechie";
+    private String secret = "collins";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
