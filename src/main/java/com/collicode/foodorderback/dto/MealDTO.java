39e03acca39361b30514e3cac900ad5386a7ddea
c-kiplimo <kiplimocollins855@gmail.com>
2023-06-22 20:10:08 +0300

    mealDtos FinalOrderDtos

---
+++ src/main/java/com/collicode/foodorderback/dto/MealDTO.java
@@ -0,0 +1,101 @@
+package com.collicode.foodorderback.dto;
+
+
+import com.collicode.foodorderback.model.Meal;
+import com.collicode.foodorderback.model.MealType;
+
+public class MealDTO {
+	
+ 	public Long id;
+	private MealType mealType;
+ 	private String name;
+ 	private int price;
+ 	
+ 	//@JsonIgnoreProperties("hibernateLazyInitializer")
+ 	//private FileSystemResource imageFSR;
+ 	
+ 	private String image;
+ 	private String imageName;
+ 	
+ 	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public MealDTO() {
+ 		
+ 	}
+ 	
+ 	public MealDTO(Meal meal) {
+ 		this(meal.getId(), meal.getMealType(), meal.getName(), meal.getPrice(), meal.getImage(), meal.getImageName());
+ 	}
+ 	
+ 	public MealDTO(Long id, MealType mealType, String name, int price, String image, String imageName) {
+ 		this.id = id;
+ 		this.mealType = mealType;
+ 		this.name = name;
+ 		this.price = price;
+ 		this.image = image;
+ 		this.imageName = imageName;
+ 	}
+ 	
+ 	/*public MealDTO(Meal meal) {
+
+ 		this.id = meal.getId();
+ 		this.mealType = meal.getMealType();
+ 		this.name = meal.getName();
+ 		this.price = meal.getPrice();
+ 	} */
+
+	/*public FileSystemResource getImageFSR() {
+		return imageFSR;
+	}
+
+	public void setImageFSR(FileSystemResource imageFSR) {
+		this.imageFSR = imageFSR;
+	}*/
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public MealType getMealType() {
+		return mealType;
+	}
+
+	public void setMealType(MealType mealType) {
+		this.mealType = mealType;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
