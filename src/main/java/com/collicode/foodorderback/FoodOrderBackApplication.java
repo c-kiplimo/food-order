d36e6096970b9d0cf737b49213d63c3fcd5e7dbf
c-kiplimo <kiplimocollins855@gmail.com>
2023-06-22 05:59:30 +0300

    first commit

---
+++ src/main/java/com/collicode/foodorderback/FoodOrderBackApplication.java
@@ -0,0 +1,13 @@
+package com.collicode.foodorderback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collicode/foodorderback/FoodOrderBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collicode.foodorderback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
